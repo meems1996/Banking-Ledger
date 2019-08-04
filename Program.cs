72a5e76faf0d22f9c9913a6592461b8370610e16
Maria Shimkovska <mimi@Marias-MacBook-Pro.local>
2019-08-03 20:14:06 -0700

    Connected to Redis db

--- Program.cs
+++ Program.cs
@@ -3,17 +3,30 @@ using System.Net;
 using Enyim.Caching;
 using Enyim.Caching.Configuration;
 using Enyim.Caching.Memcached;
+using ServiceStack.Redis;
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.DataAnnotations;
 
 namespace BankLedger
-{
+{    
     class  StartApplication{
         public void DisplayOptions() {
-            Console.WriteLine("\nWelcome to Bank Ledger. Choose to sign up or login by typing 1 or 2.");
-            Console.WriteLine("To logout just type logout.");
+            Console.ForegroundColor = ConsoleColor.White;
 
-            String options = ("\n1. Sign Up     2. Login");
+            var signuptext = "1. Sign up";
+            var logintext = "2. Log In";
 
-            Console.WriteLine(options);
+            Console.WriteLine("=============================================");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("           Welcome to Bank Ledger");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("=============================================");
+
+            Console.Write(signuptext);
+            Console.Write("     ");
+            Console.WriteLine(logintext);
         }
 
         public void AskForOption() {
@@ -26,18 +39,30 @@ namespace BankLedger
             }
         }
         public void SignUp() {
+            var retypepassword = "Retype Password";
+
+            Console.WriteLine("\nSign Up By Choosing a Username and a Password.");
             Console.WriteLine("\nChoose Username:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             String chooseUsername = Console.ReadLine();
+            Console.ResetColor();
             Console.WriteLine("Choose Password");
             String choosePassword = Console.ReadLine();
-            Console.WriteLine("Retype Password");
+            Console.WriteLine(retypepassword);
+            Console.ForegroundColor = ConsoleColor.Yellow;
             String retypePassword = Console.ReadLine();
+            Console.ResetColor();
         }
         public void LogIn() {
+            Console.WriteLine("\nLog in to your account by typing in your username and password.");
             Console.WriteLine("\nUsername:");
+            Console.ForegroundColor = ConsoleColor.Red;
             String username = Console.ReadLine();
+            Console.ResetColor();
             Console.WriteLine("\nPassword:");
+            Console.ForegroundColor = ConsoleColor.Red;
             String password = Console.ReadLine();
+            Console.ResetColor();
 
         }
     }
@@ -46,11 +71,16 @@ namespace BankLedger
     {
         static void Main(string[] args)
         {
+            var redisManager = new RedisManagerPool("localhost:6379");
+            
+            using (var client = redisManager.GetClient()) {
+                //client.Set("foo", "bar");
+                Console.WriteLine("foo={0}", client.Get<String>("mystring"));
+            }
+
             StartApplication start = new StartApplication();
             start.DisplayOptions();
             start.AskForOption();
-
         }
     }
-
 }
